afa6d12f8051a977afc0df9ab90420ab9bdea3b4
skyeler yearsley <s2krighty@gmail.com>
2023-10-16 13:00:23 -0500

    Start process of functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 
 
 function App() {
+ const [ songs,setSongs] = useState([]);
+ useEffect(()=> {
+  getAllSongs();
+ },[]);
+
+ async function getAllSongs(){
+  const response = await axios.get('https://localhost:7221/api/Songs');
+  setSongs(response.data);
+  console.log(songs);
+ }
+ 
+
+
+
+
+
+
+
+
   return (
     <div >
      
-   sadasdas
+     <button onClick={()=>getAllSongs()}> Get All Songs </button>
+  
     </div>
   );
 }
